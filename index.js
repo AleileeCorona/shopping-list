44c9f67f073c76cb2b690fc6b37343572492d7c5
AleileeCorona <66973205+AleileeCorona@users.noreply.github.com>
2020-06-26 14:45:11 -0700

    Changed the coding in line 22 of the js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function handleAddSubmission() {
     $('#js-shopping-list-form').on('submit', event => {
         event.preventDefault();
         event.stopPropagation();
-        let inputVal = $(".js-shopping-list-entry").val();
+        let inputVal = $("#shopping-list-entry").val();
         $('ul.shopping-list').append(`<li>
         <span class="shopping-item">${inputVal}</span>
         <div class="shopping-item-controls">
